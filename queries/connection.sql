666e64c2be6dcb20530a919d1581fbc7e04e363a
bgroup51 <itsjustm3h@gmail.com>
2021-06-15 18:39:15 +0300

    del

--- queries/connection.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [Preferences_2021] (
-    [id_ep]   INT NOT NULL,
-    [id_user] INT NOT NULL,
-    PRIMARY KEY (id_ep, id_user)
-);
--- queries/pref.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [Preferences_2021] (
-    [id_ep]   SMALLINT NOT NULL,
-    [id_user] SMALLINT NOT NULL,
-    PRIMARY KEY (id_ep , id_user ),
-    FOREIGN KEY (id_user) REFERENCES [Users_2021] (id)
-);
